b0f210abb4a86541a12f9613c4f7307d86def97b
arianna choza <unachoza@gmail.com>
2020-08-25 22:19:16 -0400

    add client works, adds to dom of client list and to local storage

--- scriptduex.js
+++ scriptduex.js
@@ -17,8 +17,13 @@ console.log('this is the modal', modal);
 const openButton = document.getElementById('open-model-button');
 const modalShow = document.getElementById('show-modal');
 const modalClose = document.getElementById('close-modal');
-// console.log(openButton);
-// Control Navigation Animation
+const addClientForm = document.getElementById('client-form');
+const clientNameElement = document.getElementById('client-name');
+const clientContainer = document.getElementById('client-container');
+
+// Array for list of clients
+let clients = [];
+
 const navAnimation = (direction1, direction2) => {
   navItems.forEach((nav, i) => {
     nav.classList.replace(`slide-${direction1}-${i + 1}`, `slide-${direction2}-${i + 1}`);
@@ -57,3 +62,45 @@ modalClose.addEventListener('click', () => {
   modal.classList.remove('show-modal');
 });
 window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
+
+// Build Client List
+const buildClientList = () => {
+  clientContainer.textContent = '';
+  clients.forEach((client) => {
+    const { name } = client;
+    const clientItem = document.createElement('div');
+    clientItem.classList.add('drag-item');
+    clientItem.textContent = name;
+    clientContainer.appendChild(clientItem);
+  });
+};
+const fetchClientList = () => {
+  if (localStorage.getItem('clients')) {
+    clients = JSON.parse(localStorage.getItem('clients'));
+  } else {
+    clients = [
+      {
+        name: 'Choza ',
+      },
+    ];
+    localStorage.setItem('clients', JSON.stringify(clients));
+  }
+  buildClientList();
+};
+
+// Adding Clients to Client List
+const addClient = (e) => {
+  e.preventDefault();
+  const nameValue = clientNameElement.value;
+  const client = {
+    name: nameValue,
+    // hours: clientHoursValue,
+  };
+  clients.push(client);
+  localStorage.setItem('clients', JSON.stringify(clients));
+  fetchClientList();
+  addClientForm.reset();
+  clientNameElement.focus();
+};
+
+addClientForm.addEventListener('submit', addClient);
