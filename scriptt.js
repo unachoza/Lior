e88e3b8f44c6aecadb75eab89239b7f3b761803d
arianna choza <unachoza@gmail.com>
2020-10-08 19:02:11 -0700

    service worker with https

--- scriptt.js
+++ scriptt.js
@@ -1,115 +1,14 @@
-/*
-Copyright 2015, 2019 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
-
-// Incrementing OFFLINE_VERSION will kick off the install event and force
-// previously cached resources to be updated from the network.
-const OFFLINE_VERSION = 1;
-const CACHE_NAME = 'offline';
-// Customize this with a different URL if needed.
-const OFFLINE_URL = 'offline.html';
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    (async () => {
-      const cache = await caches.open(CACHE_NAME);
-      // Setting {cache: 'reload'} in the new request will ensure that the response
-      // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
-      await cache.add(new Request(OFFLINE_URL, { cache: 'reload' }));
-    })()
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    (async () => {
-      // Enable navigation preload if it's supported.
-      // See https://developers.google.com/web/updates/2017/02/navigation-preload
-      if ('navigationPreload' in self.registration) {
-        await self.registration.navigationPreload.enable();
-      }
-    })()
-  );
-
-  // Tell the active service worker to take control of the page immediately.
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  // We only want to call event.respondWith() if this is a navigation request
-  // for an HTML page.
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      (async () => {
-        try {
-          // First, try to use the navigation preload response if it's supported.
-          const preloadResponse = await event.preloadResponse;
-          if (preloadResponse) {
-            return preloadResponse;
-          }
-
-          const networkResponse = await fetch(event.request);
-          return networkResponse;
-        } catch (error) {
-          // catch is only triggered if an exception is thrown, which is likely
-          // due to a network error.
-          // If fetch() returns a valid HTTP response with a response code in
-          // the 4xx or 5xx range, the catch() will NOT be called.
-          console.log('Fetch failed; returning offline page instead.', error);
-
-          const cache = await caches.open(CACHE_NAME);
-          const cachedResponse = await cache.match(OFFLINE_URL);
-          return cachedResponse;
-        }
-      })()
-    );
-  }
-
-  // If our if() condition is false, then this fetch handler won't intercept the
-  // request. If there are any other fetch handlers registered, they will get a
-  // chance to call event.respondWith(). If no fetch handlers call
-  // event.respondWith(), the request will be handled by the browser as if there
-  // were no service worker involvement.
-});
-
-// On install, cache some stuff
-addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open('core').then(function (cache) {
-      cache.add(new Request('offline.html'));
-      return;
+if ('serviceWorker' in navigator) {
+  // Register service worker
+  navigator.serviceWorker
+    .register('sw.js')
+    .then(function (reg) {
+      console.log('SW registration succeeded. Scope is ' + reg.scope);
     })
-  );
-});
-// listen for requests
-addEventListener('fetch', function (event) {
-  // Get the request
-  var request = event.request;
-
-  // HTML files
-  // Network-first
-  if (request.headers.get('Accept').includes('text/html')) {
-    event.respondWith(
-      fetch(request)
-        .then(function (response) {
-          return response;
-        })
-        .catch(function (error) {
-          return caches.match('offline.html');
-        })
-    );
-  }
-});
-
+    .catch(function (err) {
+      console.error('SW registration failed with error ' + err);
+    });
+}
 //Initializing Variables
 let savedClientList = [];
 let savedClientHours = [];
@@ -330,4 +229,4 @@ navItems.forEach((nav) => {
 });
 
 // fetchClientListFromLocalStorage();
-const makeHashTableForLocalStorage = () => {};
+// const makeHashTableForLocalStorage = () => {}
